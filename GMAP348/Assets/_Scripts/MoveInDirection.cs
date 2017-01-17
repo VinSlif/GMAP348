91efc274f5036d9a4c0724b809011227b43b6a87
VinSlif <Vincent.Slifer@gmail.com>
2017-01-16 20:24:12 -0500

    Core Mechanics Coding
    - Modified FPS Rigidbody Standard Asset to work with new mechanics
    - Connected new control scheme to UI
    - Linked FOV to distance from wall
    - Added level resets
    - Added Moving Wall
    - Integrated Level Design

---
+++ GMAP348/Assets/_Scripts/MoveInDirection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveInDirection : MonoBehaviour {
+
+	public Vector3 direction = new Vector3(0, 0, 0);
+	public float speed = 5.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(direction.x * speed * Time.deltaTime,
+			direction.y * speed * Time.deltaTime,
+			direction.z * speed * Time.deltaTime);
+	}
+}
