d821df27adbddfed73993a7e47a4f2672b21214b
VinSlif <Vincent.Slifer@gmail.com>
2017-02-06 10:53:37 -0500

    Multiple NavMesh Agents
    Got different types of navmesh agents to work on a single navmesh

---
+++ GMAP348/Assets/_Scripts/MoveTo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class MoveTo : MonoBehaviour {
+	public Transform goal;
+
+	void Start() {
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.destination = goal.position; 
+	}
+}
